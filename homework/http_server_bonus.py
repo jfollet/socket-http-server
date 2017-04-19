5dcda6c35e977c23e9062964e1ad78c9c6b6d082
Jesse Follet <jesse.follet@yahoo.com>
2017-04-19 04:50:59 +0000

    Created http_server_bonus to work on the bonus objectives.  Finished the html formatting of the directory links.

---
+++ homework/http_server_bonus.py
@@ -0,0 +1,168 @@
+import mimetypes
+import os
+import socket
+import sys
+
+
+def response_ok(body=b"this is a pretty minimal response", mimetype=b"text/plain"):
+    """
+    returns a basic HTTP response
+
+    Ex:
+        response_ok(
+            b"<html><h1>Welcome:</h1></html>",
+            b"text/html"
+        ) ->
+
+        b'''
+        HTTP/1.1 200 OK
+        Content-Type: text/html
+
+        <html><h1>Welcome:</h1></html>
+        '''
+
+    """
+
+    resp = []
+    resp.append(b"HTTP/1.1 200 OK")
+    resp.append(b"Content-Type: " + mimetype)
+    resp.append(b"")
+    resp.append(body)
+    return b"\r\n".join(resp)
+
+
+def response_method_not_allowed():
+    """returns a 405 Method Not Allowed response"""
+    resp = []
+    resp.append("HTTP/1.1 405 Method Not Allowed")
+    resp.append("")
+    return "\r\n".join(resp).encode('utf8')
+
+
+def response_not_found():
+    """returns a 404 Not Found response"""
+
+    resp = []
+    resp.append("HTTP/1.1 404 Not Found")
+    resp.append("")
+    return "\r\n".join(resp).encode('utf8')
+
+
+def parse_request(request):
+    first_line = request.split("\r\n", 1)[0]
+    method, uri, protocol = first_line.split()
+    if method != "GET":
+        raise NotImplementedError("We only accept GET")
+    return uri
+
+
+def list_dir_in_html(uri, uri_file):
+    content = "<!DOCTYPE html>\r\n"
+    content += "<html>\r\n"
+    content += "<body>\r\n"
+    content += "<ul>\r\n"
+    content += "".join(
+        "<li><a href=\"{}{}\">{}</a></li>\r\n".format(uri, file, file) for file in (os.listdir(uri_file)))
+    content += "</ul>\r\n"
+    content += "</body>\r\n"
+    content += "</html>\r\n"
+    content = content.encode()
+    return content
+
+
+def resolve_uri(uri):
+    """
+    This method should return appropriate content and a mime type.
+
+    If the requested URI is a directory, then the content should be a
+    plain-text listing of the contents with mimetype `text/plain`.
+
+    If the URI is a file, it should return the contents of that file
+    and its correct mimetype.
+
+    If the URI does not map to a real location, it should raise an
+    exception that the server can catch to return a 404 response.
+
+    Ex:
+        resolve_uri('/a_web_page.html') -> (b"<html><h1>North Carolina...",
+                                            b"text/html")
+
+        resolve_uri('/images/sample_1.png')
+                        -> (b"A12BCF...",  # contents of sample_1.png
+                            b"image/png")
+
+        resolve_uri('/') -> (b"images/, a_web_page.html, make_type.py,...",
+                             b"text/plain")
+
+        resolve_uri('/a_page_that_doesnt_exist.html') -> Raises a NameError
+
+    """
+
+    uri_file = os.path.join(os.path.dirname(__file__), "webroot", uri[1:])
+    if not os.path.exists(uri_file):
+        raise NameError
+
+    mime_type = "text/plain"
+    if os.path.isdir(uri_file):
+        content = list_dir_in_html(uri, uri_file)
+        mime_type = "text/html"
+        # content = ", ".join(os.listdir(uri_file)).encode()
+    else:
+        pathname, filename = os.path.split(uri_file)
+        mime_type, _ = mimetypes.guess_type(filename)
+        if mime_type is None:
+            if os.path.splitext(filename)[1] is ".py":
+                mime_type = "text/x-python"
+        f = open(uri_file, "rb")
+        content = f.read()
+        f.close()
+    mime_type = mime_type.encode()
+    return content, mime_type
+
+
+def server(log_buffer=sys.stderr):
+    address = ('127.0.0.1', 10000)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("making a server on {0}:{1}".format(*address), file=log_buffer)
+    sock.bind(address)
+    sock.listen(1)
+
+    try:
+        while True:
+            print('waiting for a connection', file=log_buffer)
+            conn, addr = sock.accept()  # blocking
+            try:
+                print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                request = ''
+                while True:
+                    data = conn.recv(1024)
+                    request += data.decode('utf8')
+                    if len(data) < 1024:
+                        break
+
+                try:
+                    uri = parse_request(request)
+                except NotImplementedError:
+                    response = response_method_not_allowed()
+                else:
+                    try:
+                        content, mime_type = resolve_uri(uri)
+                    except NameError:
+                        response = response_not_found()
+                    else:
+                        response = response_ok(content, mime_type)
+
+                print('sending response', file=log_buffer)
+                conn.sendall(response)
+            finally:
+                conn.close()
+
+    except KeyboardInterrupt:
+        sock.close()
+        return
+
+
+if __name__ == '__main__':
+    server()
+    sys.exit(0)
